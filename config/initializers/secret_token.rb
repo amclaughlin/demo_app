1be2c691b809408f6a8f8fd1f376a66dda343ddc
anthony <anthony.mclaughlin@invisionwfm.com>
2011-11-10 09:30:01 +0000

    First commit amcl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f0c67069f10c29a02089de11728a38729164901bf45ac938db6ee29efa1eb97012b88f68ef6738c67983f0135c9991aa8eb4c2b68ff3d6b729927bdd83caaa60'
